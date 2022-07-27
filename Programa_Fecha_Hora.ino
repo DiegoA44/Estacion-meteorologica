5938c7c776e8a7726ce3fc80830356401e3cd4d9
DiegoA44 <110132693+DiegoA44@users.noreply.github.com>
2022-07-27 16:13:45 -0500

    Add files via upload

---
+++ Programa_Fecha_Hora.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <RTClib.h>
+ 
+// Declaramos un RTC DS3231
+RTC_DS3231 rtc;
+ 
+void setup () {
+ Serial.begin(9600);
+ 
+ delay(3000);
+ 
+ // Comprobamos si tenemos el RTC conectado
+ if (! rtc.begin()) {
+ Serial.println("No hay un módulo RTC");
+ while (1);
+ }
+ 
+ // Ponemos en hora, solo la primera vez, luego comentar y volver a cargar.
+ // Ponemos en hora con los valores de la fecha y la hora en que el sketch ha sido compilado.
+ //rtc.adjust(DateTime(F(), F(__TIME__)));
+}
+ 
+void loop () {
+ DateTime now = rtc.now();
+ 
+ Serial.print(now.day());
+ Serial.print('/');
+ Serial.print(now.month());
+ Serial.print('/');
+ Serial.print(now.year());
+ Serial.print(" ");
+ Serial.print(now.hour());
+ Serial.print(':');
+ Serial.print(now.minute());
+ Serial.print(':');
+ Serial.print(now.second());
+ Serial.println();
+ delay(3000);
+}
